68f065d9ff4aa02abe540a0c7395842094cf5fc5
Malachi Egbugha <malachi.egbugha@gmail.com>
2025-12-15 14:33:23 +0000

    ghmlf

--- src/Utils/ApiConfig.js
+++ src/Utils/ApiConfig.js
@@ -37,7 +37,7 @@ else if (baseChecker === "garsh.ehealthcareconnect.com") {
 else if (baseChecker === "20.164.19.147") {
   FacilityName = "TEST HOSPITAL";
 } else {
-  FacilityName = "GENERAL HOSPITAL, FUNTUA";
+  FacilityName = "GENERAL HOSPITAL, MALUMFASHI";
 }
 
 export const token = localStorage.getItem("token");
